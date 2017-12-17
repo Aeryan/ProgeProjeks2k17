5259026b8bde64f7eebaf1639d1efbce5c41ce6b
Void <raunoita@gmail.com>
2017-12-17 16:30:10 +0200

    A floating head, detects mouseover

--- select.py
+++ select.py
@@ -1,4 +1,5 @@
 from sharedvars import *
+import pygame
 
 
 def init():
@@ -6,11 +7,21 @@ def init():
 
 
 def update():
-    pass
+    if var.head_start[0] <= pygame.mouse.get_pos()[0] <= var.head_start[0] + var.head1.get_width() * 2 \
+            and var.head_start[1] <= pygame.mouse.get_pos()[1] <= var.head_start[1] + var.head1.get_height():
+        var.hover = "head"
+    else:
+        var.hover = ""
 
 
 def draw(screen):
-    pass
+    screen.fill((255, 255, 255), (0, 0, var.screen_w, var.screen_h))
+    if var.hover == "head":
+        screen.blit(var.head1r, (100, 100))
+        screen.blit(pygame.transform.flip(var.head1r, True, False), (100 + var.head1r.get_width(), 100))
+    else:
+        screen.blit(var.head1, (100, 100))
+        screen.blit(pygame.transform.flip(var.head1, True, False), (100 + var.head1.get_width(), 100))
 
 
 def on_event(event):
--- sharedvars.py
+++ sharedvars.py
@@ -5,16 +5,21 @@ class Var:
 
     def __init__(self):
         self.screen_w = 1280
-        self.screen_h = 500
+        self.screen_h = 600
         self.hand1 = None
         self.head1 = None
+        self.head1r = None
         self.foot1 = None
         self.back1 = None
         self.shoulder1 = None
         self.imgDir = "Images"
 
+        self.hover = ""
+        self.head_start = (100, 100)
+
     def image_set(self):
         self.head1 = pygame.image.load(self.imgDir + "\head1.png").convert_alpha()
+        self.head1r = pygame.image.load(self.imgDir + "\head1red.png").convert_alpha()
         self.foot1 = pygame.image.load(self.imgDir + "\\foot1.png").convert()
         self.hand1 = pygame.image.load(self.imgDir + "\hand1.png").convert_alpha()
         self.back1 = pygame.image.load(self.imgDir + "\\back1.png").convert_alpha()
